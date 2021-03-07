0bd1055c65a2ba7b48a8758d60ebd94c3bf3c04e
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-03-07 16:14:47 -0300

    validando formulario

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,8 +3,15 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form @submit.prevent="submit">
-          <v-text-field label="Email" type="Email"> </v-text-field>
+        <v-form @submit.prevent="submit" ref="signUpForm" v-model="formValidity">
+          <v-text-field
+            label="Email"
+            type="Email"
+            v-model="email"
+            :rules="emailRules"
+            required
+          >
+          </v-text-field>
           <v-autocomplete
             label="witch browser do you use?"
             :items="browsers"
@@ -39,8 +46,18 @@
             </v-date-picker>
           </v-dialog>
 
-          <v-checkbox label="Agree to terms & conditions"></v-checkbox>
-          <v-btn type="submit" color="primary">Submit</v-btn>
+          <v-checkbox
+            label="Agree to terms & conditions"
+            v-model="agreeToTerms"
+            :rules="agreeToTermsRules"
+            required
+          ></v-checkbox>
+          <v-btn class="mr-4" type="submit" color="primary" :disabled="!formValidity">Submit</v-btn>
+          <v-btn class="mr-4" color="success" @click="validateForm">Validate Form</v-btn>
+          <v-btn class="mr-4" color="warning" @click="resetValidation"
+            >Reset Validation</v-btn
+          >
+          <v-btn color="error" @click="resetForm">Reset</v-btn>
         </v-form>
       </v-col>
     </v-row>
@@ -50,7 +67,26 @@
 <script>
 export default {
   data: () => ({
+    agreeToTerms: false,
+    agreeToTermsRules: [
+      (value) =>
+        !!value ||
+        "You must to the terms and conditions to sign up for an account.",
+    ],
+    email: "",
+    emailRules: [
+      (value) => !!value || "Email is required.",
+      (value) => value.indexOf("@") !== 0 || "Email should have a username.",
+      (value) => value.includes("@") || "Email should include an @ symbol.",
+      (value) =>
+        value.indexOf(".") - value.indexOf("@") > 1 ||
+        "Email should contain a valid domain",
+      (value) =>
+        value.indexOf(".") <= value.length - 3 ||
+        "Email should contain a valid domain extension",
+    ],
     browsers: ["Chrome", "Firefox", "Safari", "Edge", "Brave"],
+    formValidity: false,
     birthday: "",
     modal: false,
   }),
@@ -59,6 +95,18 @@ export default {
     submit() {
       alert("Hello world");
     },
+
+    resetForm() {
+      this.$refs.signUpForm.reset();
+    },
+
+    resetValidation() {
+      this.$refs.signUpForm.resetValidation();
+    },
+
+    validateForm() {
+        this.$refs.signUpForm.validate()
+    }
   },
 };
 </script>
